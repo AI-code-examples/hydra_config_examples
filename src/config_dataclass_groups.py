e8eef9642b500c991d4751bc41d6ebd3e01950fd
zYx.Tom <ygpfr74@hotmail.com>
2023-06-27 15:38:19 +0800

    Create(config_dataclass_groups): 使用继承与配置分组

---
+++ src/config_dataclass_groups.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#  -*- encoding: utf-8 -*-
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+-----------------------------------------------
+@Software   :   PyCharm
+@Project    :   hydra_config_examples
+@File       :   config_dataclass_groups
+@Version    :   v0.1
+@Time       :   2023/6/27 15:06
+@License    :   (C)Copyright    2018-2023,  zYx.Tom
+@Reference  :   
+@Description:   
+@Thought    :
+"""
+from dataclasses import dataclass
+from typing import Any
+
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import MISSING
+from omegaconf import OmegaConf
+
+
+@dataclass
+class DBConfig:  # 将共用字段抽取出来作为父类
+    port: int = MISSING  # 没有默认值的要排在前面，MISSING与???等价
+    driver: str = MISSING
+    host: str = 'localhost'
+
+
+@dataclass
+class MySQLConfig(DBConfig):
+    driver: str = "mysql"
+    port: int = 3306
+
+
+@dataclass
+class PostGreSQLConfig(DBConfig):
+    driver: str = "postgresql"
+    port: int = 5432
+    timeout: int = 10
+
+
+@dataclass
+class Config:  # `db`的参数运行时添加
+    db: Any
+
+
+# Create config group `db` with options 'mysql' and 'postgreqsl'
+cs = ConfigStore.instance()
+cs.store(name="config_db", node=Config)
+cs.store(group="db", name="mysql", node=MySQLConfig)
+cs.store(group="db", name="postgresql", node=PostGreSQLConfig)
+
+
+@hydra.main(version_base=None, config_name="config_db")
+def main(cfg: Config) -> None:
+    """
+
+    运行方式需要添加 `+db=`，因为`db`没有默认值
+    `python src/config_dataclass_groups.py +db=mysql`
+
+    :param cfg:
+    :return:
+    """
+    print(OmegaConf.to_yaml(cfg))
+    pass
+
+
+# ----------------------------------------------------------------------
+# 小结
+if __name__ == '__main__':
+    main()
